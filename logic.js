981b95aac1d9f95469597d447fc8db1cb19c9a8a
gabriel pacheco <matheusinho@hotmail.com>
2023-11-01 19:30:37 -0300

    The game doesn't work with numbers, but with strings (rock, paper and scissors).

--- logic.js
+++ logic.js
@@ -1,26 +1,33 @@
 
 
 // Function to load the HTML before JavaScript.
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-  // Player's guide
-  console.log("\n0 = Rock\n1 = Paper\n2 = Scissors\n ");
+document.addEventListener("DOMContentLoaded", function(event) {
 
   // Functions to obtain the player's choice
   function playerChoice() {
-    return parseInt(prompt("Choose a number: 0, 1 or 2?"))
+    return prompt("Paper, Rock or Scissors?").toLowerCase();
   };
 
   // Functions to obtain the computer's choice
   function computerChoice() {
-    return Math.floor(Math.random() * 3);
+    a = Math.floor(Math.random() * 3);
+
+    // Return Rock, Paper or Scissors based on the random number generated
+    if (a === 0) {
+      return "rock";
+    } else if (a === 1) {
+      return "paper";
+    } else if (a === 2) {
+      return "scissors";
+    }
   };
+
 
   // Function that loops the game and evaluates the rounds
   function game() {
     // Scores
     let playerScore = 0;
-    let computerScore = 0;  
+    let computerScore = 0;
 
     // As long as none of the scores is equal to 3
     while (playerScore <= 2 && computerScore <= 2) {
@@ -29,25 +36,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
       // Function to show choices and scores
       function choicesAndScores() {
-        return console.log(`\nPlayer choice = ${player}\nComputer choice = ${computer}\n\nPlayer score: ${playerScore} | Computer score: ${computerScore}\n `);
+        return console.log(`\nPlayer choice = ${player}\nComputer choice = ${computer}\n\nPlayer - ${playerScore} | ${computerScore} - Computer\n `);
       };
 
       // Player = Rock
-      if (player === 0) {
+      if (player === "rock") {
 
         // Computer = Rock
-        if (computer === 0) {
+        if (computer === "rock") {
           console.log("Draw");
         }
         
         // Computer = Paper
-        else if (computer === 1) {
+        else if (computer === "paper") {
           computerScore++;
           choicesAndScores();
         }
         
         // Computer = Scissors
-        else if (computer === 2) {
+        else if (computer === "scissors") {
           playerScore++;
           choicesAndScores();
         };
@@ -55,21 +62,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
       } 
       
       // Player = Paper
-      else if (player === 1) {
+      else if (player === "paper") {
 
         // Computer = Rock
-        if (computer === 0) {
+        if (computer === "rock") {
           playerScore++;
           choicesAndScores();
         }
         
         // Computer = Paper
-        else if (computer === 1) {
+        else if (computer === "paper") {
           console.log("Draw");
         }
         
         // Computer = Scissors
-        else if (computer === 2) {
+        else if (computer === "scissors") {
           computerScore++;
           choicesAndScores();
         };
@@ -77,39 +84,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
 
       // Player = Scissors
-      else if (player === 2) {
+      else if (player === "scissors") {
         
         // Computer = Rock
-        if (computer === 0) {
+        if (computer === "rock") {
           computerScore++;
           choicesAndScores();
         }
 
         // Computer = Paper
-        else if (computer === 1) {
+        else if (computer === "paper") {
           playerScore++;
           choicesAndScores();
         }
 
         // Computer = Scissors
-        else if (computer === 2) {
+        else if (computer === "scissors") {
           console.log("Draw");
         };
       } 
-      else {
-        console.error("Choose a valid number!")
-      };
     };
 
     // Victory message
     if (playerScore === 3) {
-      console.log("PLAYER WINS")
+      console.log("\nPLAYER WINS\n ")
     } else {
-      console.log("COMPUTER WINS")
+      console.log("\nCOMPUTER WINS\n ")
     }
   };
 
   // Function call
   game();
-
+  
 });
